6fb91ffa578cf9efe5023d257ae546c987a5ee9f
Gabriel Viana <gaa.vianaa@gmail.com>
2025-06-17 10:31:27 -0300

    funcoes

---
+++ 03-funcoes.ts
@@ -0,0 +1,8 @@
+// 03-funcoes.ts
+export {};
+
+function saudacoes(nome: string) {
+  return `Olá ${nome}`;
+}
+
+console.log(saudacoes("Gabi"));
---
+++ dist/03-funcoes.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function saudacoes(nome) {
+    return `Olá ${nome}`;
+}
+console.log(saudacoes("Gabi"));
